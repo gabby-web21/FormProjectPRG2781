6ce55da818d1966e06fa72313ac35169bf1d2dac
Rowie <petersenraoul@36.gmail.com>
2024-11-02 15:05:56 +0200

    Loaded Columns and Information From Text FIle into Data Grid View

--- FormProjectPRG2781/Form1.Designer.cs
+++ FormProjectPRG2781/Form1.Designer.cs
@@ -63,6 +63,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "View All Students";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -72,6 +73,7 @@
             this.button3.TabIndex = 1;
             this.button3.Text = "Update Student Information";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label1
             // 
@@ -134,6 +136,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(746, 263);
             this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // textBox1
             // 
@@ -185,6 +188,7 @@
             this.Controls.Add(this.AddNewStudent);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- FormProjectPRG2781/Form1.cs
+++ FormProjectPRG2781/Form1.cs
@@ -17,9 +17,24 @@ namespace FormProjectPRG2781
         {
             InitializeComponent();
         }
+        DataTable table = new DataTable(); 
+        //specifying file path for text file
+        string filepath = @"C:\Users\Kirsten\source\repos\PRG2781_Project(1)\PRG2781_Project(1)\bin\Debug\students.txt";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table.Columns.Add("Student ID", typeof(int));
+            table.Columns.Add("Full Name", typeof(string));
+            table.Columns.Add("Age", typeof(int));
+            table.Columns.Add("Course", typeof(string));
+
+            dataGridView1.DataSource = table;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //declaring and initializing variables with textboxes to refer to them. 
             string studentID = textBox1.Text;
             string studName = textBox2.Text;
             int studAge = int.Parse(textBox3.Text); 
@@ -27,7 +42,8 @@ namespace FormProjectPRG2781
 
             try
             {
-                string filepath = @"C:\Users\Kirsten\source\repos\PRG2781_Project(1)\PRG2781_Project(1)\bin\Debug\students.txt"; 
+                
+                //using streamwriter to write to file 
                 using (StreamWriter writer = new StreamWriter(filepath, true))
                 {
                     writer.WriteLine($"{studentID}, {studName}, {studAge}, {course}"); 
@@ -35,11 +51,58 @@ namespace FormProjectPRG2781
 
                 MessageBox.Show("Student Details Saved Successfully"); 
             }
+            //error message displayed if try doesnt work
             catch (Exception ex)
             {
                MessageBox.Show("Error, data not saved" + ex.Message);
                 throw;
             }
+
+            textBox1.Text = " ";
+            textBox2.Text = " ";
+            textBox3.Text = " ";
+            textBox4.Text = " ";
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string[] allStudents = File.ReadAllLines(filepath); 
+            string[] allValues;
+            for (int i = 0; i < allStudents.Length; i++)
+            {
+                allValues = allStudents[i].ToString().Split(','); 
+                string[] row = new string[allValues.Length];
+
+                for (int j = 0; j < allValues.Length; j++)
+                {
+                    row[j] = allValues[j].Trim(); 
+                }
+                try
+                {
+                    table.Rows.Add(row);
+
+                }
+                catch (Exception except)
+                {
+                    MessageBox.Show(except.Message); 
+                    throw;
+                }
+                
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
