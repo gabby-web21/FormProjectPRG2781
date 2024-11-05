81456e78a7ac3136f7a3ae525da0d925ce64210f
Rowie <petersenraoul@36.gmail.com>
2024-11-05 21:58:35 +0200

    Functionality changes and checks

--- FormProjectPRG2781/Form1.cs
+++ FormProjectPRG2781/Form1.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO; 
+using System.IO;
+using System.Drawing.Design;
 
 namespace FormProjectPRG2781
 {
@@ -20,12 +21,10 @@ namespace FormProjectPRG2781
         }
         DataTable table = new DataTable();
         //specifying file path for text file
+        string filepath = @"C:\Users\Kirsten\source\repos\FormProjectPRG2781\FormProjectPRG2781\Resources\Students.txt";
 
-        string relativePath = @"Resources\students.txt";
-
-        string filepath;
-        //try open or create file 
-
+        
+        
         private void Form1_Load(object sender, EventArgs e)
         {
             table.Columns.Add("Student ID", typeof(int));
@@ -36,117 +35,137 @@ namespace FormProjectPRG2781
             dataGridView1.DataSource = table;
             dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
 
+            
 
 
-            filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativePath);
-
-            if (!File.Exists(filepath))
-            {
-                MessageBox.Show("File not found, creating new file");
-                //File.Create(filepath).Close();
-            }
-            else
-            {
-                MessageBox.Show("File has been found!"); 
-            }
-            MessageBox.Show("Filepath: " + filepath); 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
-            //declaring and initializing variables with textboxes to refer to them. 
-            string studentID = textBox1.Text;
-            string studName = textBox2.Text;
-            int studAge; 
-            string course = textBox4.Text;
-
-            //creating validation for age to make sure a numeric value is entered
-            if (!int.TryParse(textBox3.Text, out studAge))
+            if (string.IsNullOrEmpty(filepath))
             {
-                
-                MessageBox.Show("Please enter a numeric value for age e.g. 19");
-                return;
+                MessageBox.Show("File path is not set");
+                return; 
             }
-            else
+
+            //Creating a filestream to open or create file at specified filepath and specificying that the file should be open for 
+            //appending, as well as indicating that the file will be opened with writing access.  
+            using (FileStream myStream = new FileStream(filepath, FileMode.Append, FileAccess.Write))
             {
-                studAge = int.Parse(textBox3.Text);
+                using (StreamWriter writer = new StreamWriter(myStream))
+                {
+                    //declaring and initializing variables with textboxes to refer to them. 
+                    //adding Trim function to eliminate any whitespace in between inputs
+                    string studentID = textBox1.Text.Trim();
+                    string studName = textBox2.Text.Trim();
+                    int studAge;
+                    string course = textBox4.Text.Trim();
+                    //creating validation for age to make sure a numeric value for age is entered
+                    if (!int.TryParse(textBox3.Text, out studAge))
+                    {
+
+                        MessageBox.Show("Please enter a numeric value for age e.g. 19");
+                        return;
+                    }
+                    else
+                    {
+                        studAge = int.Parse(textBox3.Text.Trim());
+                    }
+
+
+                    if (string.IsNullOrWhiteSpace(studentID) || string.IsNullOrWhiteSpace(studName) || string.IsNullOrWhiteSpace(course) || studAge < 0)
+                    {
+                        MessageBox.Show("Please enter valid information in all fields required (ID, Name, Age, Course");
+                        return;
+                    }
+                        try
+                        {
+                            writer.WriteLine($"{studentID}, {studName}, {studAge}, {course}");
+
+                            MessageBox.Show("Student Details Saved Successfully");
+                        }
+                        //error message displayed if try doesnt work
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error, data not saved" + ex.Message);
+                        }
+
+                        textBox1.Clear();
+                        textBox2.Clear();
+                        textBox3.Clear();
+                        textBox4.Clear();
+                }
             }
             
 
-            if (string.IsNullOrWhiteSpace(studentID) || string.IsNullOrWhiteSpace(studName) || string.IsNullOrWhiteSpace(course))
-            {
-                
-
-                MessageBox.Show("Please enter information in all fields required (ID, Name, Age, Course");
-                return; 
-            }
-            else
-            {
-            try
-            {
-                
-                //using streamwriter to write to file 
-                using (StreamWriter writer = new StreamWriter(filepath, true))
-                {
-                    writer.WriteLine($"{studentID}, {studName}, {studAge}, {course}"); 
-                }
-
-                MessageBox.Show("Student Details Saved Successfully"); 
-            }
-            //error message displayed if try doesnt work
-            catch (Exception ex)
-            {
-                    MessageBox.Show("Error, data not saved" + ex.Message); 
-            }
-
-            textBox1.Text = "";
-            textBox2.Text = "";
-            textBox3.Text = "";
-            textBox4.Text = "";
-
-                
-            }
+            
         }
 
 
         private void button2_Click(object sender, EventArgs e)
         {
-            table.Rows.Clear(); 
-            string[] allStudents = File.ReadAllLines(filepath); 
 
-            foreach(string student in allStudents)
+            if (File.Exists(filepath))
             {
-                string[] allValues = student.Split(',').Select(value => value.Trim()).ToArray();
-                if (allValues.Length == table.Columns.Count)
+                table.Rows.Clear();
+                try
                 {
-                    try
+                    using (StreamReader reader = new StreamReader(filepath))
                     {
-                        table.Rows.Add(allValues);
+                        string txt;
+                        while ((txt = reader.ReadLine()) != null)
+                        {
+                                string[] allValues = txt.Split(',').Select(value => value.Trim()).ToArray();
+
+                            if (allValues.Length == table.Columns.Count)
+                            {
+                                try
+                                {
+                                  table.Rows.Add(allValues);
+                                }
+                                catch (Exception eBug)
+                                {
+                                    MessageBox.Show("Error adding rows" + eBug.Message);
+                                    return;
+                                }
+
+                            }
+                            else
+                            {
+                                MessageBox.Show("Text file error");
+
+                            }
+
+                        }
                     }
-                    catch (Exception eBug)
-                    {
-                        MessageBox.Show("Enter valid ID number" + eBug.Message); 
-                        return;
-                    }
-                   
                 }
-                else
+                catch (Exception ex)
                 {
-                    MessageBox.Show("Text file error"); 
-                    
+                    MessageBox.Show("Error reading file" + ex.Message); 
+                    throw;
                 }
-            } 
-           
+
+
+            }
+            else
+            {
+                MessageBox.Show("File does not exist"); 
+            }
+
+
         }
 
         //this method is for the information to autofill in the textboxes for convenience when editing or updating
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            for (int i = 0; i < dataGridView1.Columns.Count; i++)
+            if (e.RowIndex >= 0)
             {
-                //string cellValue = dataGridView1.Rows
-                
+                DataGridViewRow selectedRow = dataGridView1.Rows[e.RowIndex];
+
+                textBox1.Text = selectedRow.Cells[0].Value.ToString();
+                textBox2.Text = selectedRow.Cells[1].Value.ToString();
+                textBox3.Text = selectedRow.Cells[2].Value.ToString();
+                textBox4.Text = selectedRow.Cells[3].Value.ToString();
             }
         }
 
